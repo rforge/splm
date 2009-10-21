b6d1bff0b6434abce3bdba2786f79e4c9534659d
gpiras <gpiras@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-21 01:15:14 +0000

    clean old version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/splm@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bsjktest.splm.R
+++
@@ -1,5 +0,0 @@
-`bsjktest.splm` <-
-function(x, x2, ...){
-## make this a function testing SD residuals on a panelmodel?? es. BSK test
-}
-
--- pkg/R/listw2U_Matrix.R
+++
@@ -1,5 +0,0 @@
-`listw2U_Matrix` <-
-function(lw){
-as(as(0.5 * (lw + t(lw)), "symmetricMatrix"), "CsparseMatrix")
-}
-
